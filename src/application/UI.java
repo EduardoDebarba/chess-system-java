16812d2a74073ae4cb8b829e2f6832e460b4582a
Eduardo Debarba <eduardo.debarba@hotmail.com>
2021-03-03 16:40:36 -0300

    HandLing exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,9 +7,7 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
-
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -19,7 +17,6 @@ public class UI {
 	public static final String ANSI_PURPLE = "\u001B[35m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
-
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -29,6 +26,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		String s = sc.nextLine();
 		char column = s.charAt(0);
@@ -46,7 +48,6 @@ public class UI {
 		}
 		System.out.println("  A B C D E F G H ");
 	}
-
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,18 +7,17 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-
 	private Board board;
-
+	
 	public ChessMatch() {
 		board = new Board(8, 8);
 		initialSetup();
 	}
-
+	
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int i = 0; i < board.getRows(); i++) {
-			for (int j = 0; j < board.getColumns(); j++) {
+		for (int i=0; i<board.getRows(); i++) {
+			for (int j=0; j<board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
@@ -30,26 +29,26 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
 		Piece capturedPiece = makeMove(source, target);
-		return (ChessPiece) capturedPiece;
+		return (ChessPiece)capturedPiece;
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
-	
+
 	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)) {
-			throw new ChessException("Aqui não existe uma peça na posição");
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
 		}
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-
+	
 	private void initialSetup() {
 		placeNewPiece('C', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('C', 2, new Rook(board, Color.WHITE));
@@ -66,3 +65,4 @@ public class ChessMatch {
 		placeNewPiece('D', 8, new King(board, Color.BLACK));
 	}
 }
+
